09f48c76473d1bc7a0927c3ba1a50fb856ac8b15
naveenreddymanukonda <40488776+naveenreddymanukonda@users.noreply.github.com>
2023-05-30 04:36:58 +0530

    Update spec.cy.js

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,6 +1,6 @@
 
 
- describe('cypress test suite sample',function(){
+ describe('cypress test suite sample test',function(){
 
   it('CTSS_TS01_TC01',function(){
       cy.visit('https://testkube.io');
